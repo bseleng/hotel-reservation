daa9495a69420e8a63be21dfd941543effb785c0
bseleng <bogdan.selenginskiy@multilogin.com>
2023-10-24 12:26:20 +0300

    fix: remove mongodb warning

--- db/user_store.go
+++ db/user_store.go
@@ -81,11 +81,10 @@ func (s *MongoUserStore) DeleteUser(ctx context.Context, id string) error {
 }
 
 func (s *MongoUserStore) UpdateUser(ctx context.Context, filter bson.M, params types.UpdateUserParams) error {
-	update := bson.D{
-		{
-			"$set", params.ToBSON(),
-		},
+	update := bson.M{
+		"$set": params.ToBSON(),
 	}
+
 	_, err := s.coll.UpdateOne(ctx, filter, update)
 	if err != nil {
 		return err
